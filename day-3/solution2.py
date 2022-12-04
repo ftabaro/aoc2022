2e10ba9033e59910223051b852e902348321a4ea
Francesco Tabaro <francesco.tabaro@embl.it>
2022-12-04 11:01:22 +0100

    implement tests for day 3

--- day-3/solution2.py
+++ day-3/solution2.py
@@ -1,27 +1,36 @@
 import string
 
-priority = string.ascii_lowercase + string.ascii_uppercase
-priority = list(priority)
-score = 0
-group = []
+PRIORITY = string.ascii_lowercase + string.ascii_uppercase
+PRIORITY = list(PRIORITY)
 
 def get_group_score(group):
     intersection = group[0] & group[1] & group[2]
     intersection = list(intersection)
     intersection = intersection[0]
-    group_score = priority.index(intersection) + 1
+    group_score = PRIORITY.index(intersection) + 1
     print(intersection, group_score)
     return group_score
 
-with open("input.txt") as fin:
-    for i, line in enumerate(fin):
-        if i % 3 == 0 and i > 0:
-            group_score = get_group_score(group)
-            score += group_score
-            group = []
+def main(path: str) -> int:
+    score = 0
+    group = []
 
-        rucksack = line.strip()
-        group.append(set(rucksack))
-        
-score += get_group_score(group)
-print(score)
+
+    with open(path) as fin:
+        for i, line in enumerate(fin):
+            if i % 3 == 0 and i > 0:
+                group_score = get_group_score(group)
+                score += group_score
+                group = []
+
+            rucksack = line.strip()
+            group.append(set(rucksack))
+            
+    score += get_group_score(group)
+    print(score)
+    return score
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1])
