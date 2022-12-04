78a5ec481162e1910766e178c0478752b583b67e
Francesco Tabaro <francesco.tabaro@embl.it>
2022-12-04 11:13:24 +0100

    refactor tests

--- day-4/test_day4.py
+++ day-4/test_day4.py
@@ -1,14 +1,19 @@
+import pathlib
+
 from solution1 import main as sol1
 from solution2 import main as sol2
 
+fp = pathlib.Path(__file__).resolve()
+wd = fp.parents[0]
+test_file = wd.joinpath("test.txt")
 
-def test_sol1(capsys):
+def test_day4_sol1(capsys):
     # with capsys.disabled():
-    solution = sol1("test.txt")
+    solution = sol1(test_file)
     assert solution == 2
 
 
-def test_sol2(capsys):
+def test_day4_sol2(capsys):
     # with capsys.disabled():
-    solution = sol2("test.txt")
+    solution = sol2(test_file)
     assert solution == 4
